7dadd39f9deef66abb95f46af14f9d8c68131da0
feroz <ferozali690@gmail.com>
2020-09-03 17:19:02 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import AnalogClock from "analog-clock-react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      options: {
+        width: "200px",
+        useCustomTime: true,
+        border: true,
+        borderColor: "#2e2e2e",
+        baseColor: "lightblue",
+        centerColor: "#459cff",
+        handColors: {
+          second: "green",
+          minute: "blue",
+          hour: "black",
+        },
+        seconds: 1,
+        minutes: 10,
+        hours: 22,
+      },
+    };
+  }
+  updateClock = () => {
+    let asiaTime = new Date().toLocaleString("en-US", {
+      timeZone: "Asia/Kolkata",
+    });
+    let date = new Date(asiaTime);
+
+    this.setState({
+      options: {
+        ...this.state.options,
+        seconds: date.getSeconds(),
+        minutes: date.getMinutes(),
+        hours: date.getHours(),
+      },
+    });
+  };
+  componentDidMount() {
+    this.interval = setInterval(this.updateClock, 1000);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <AnalogClock {...this.state.options} />
+      </div>
+    );
+  }
 }
 
 export default App;
